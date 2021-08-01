5923e190d67199ba3dc340f5d94278b010a89261
joaovictorbelle <joaovictorbelle@gmail.com>
2021-08-01 13:28:21 -0300

    teste expo

--- App/App.js
+++ App/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TESTE JOÃO 01/08/2021 - teste</Text>
       <StatusBar style="auto" />
     </View>
   );
