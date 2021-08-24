91ebca6337bffd275954d94925bc16e8daca8285
Wesley Adiel Alves de Lima <wesleyadiel@hotmail.com>
2021-08-24 00:06:03 -0300

    data no cadastro

--- App3/src/screens/CadastrarItemScreen.js
+++ App3/src/screens/CadastrarItemScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Linking, Image, FlatList, StyleSheet, Text, Alert} from "react-native";
 import * as firebase from "firebase";
 import {
@@ -11,6 +11,7 @@ import {
   themeColor, TextInput, Picker
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
+import DatePicker from 'react-native-datepicker'
 
 
 const styles = StyleSheet.create({
@@ -33,19 +34,28 @@ const styles = StyleSheet.create({
 	}
   });
   
-  async function salvar()
-  {
-	alert("Item salvo");
-  }
-
-
-
   export default function ({ route, navigation }) {
-	const { codigo, descricao } = route.params;
+	const { codigoParam, descricaoParam } = route.params;
 
 	const { isDarkmode, setTheme } = useTheme();
+	const [codigo, setCodigo] = useState(0);
+	const [descricao, setDescricao] = useState('');
+	const [marca, setMarca] = useState('');
+	const [numeroSerie, setNumeroSerie] = useState('');
+	const [dtCompra, setDtCompra] = useState(new Date());
+	const [dtGarantia, setDtGarantia] = useState(new Date());
+	const [setor, setSetor] = useState('');
 
+    const entityRef = firebase.firestore().collection('entities');
+    //const userID = props.extraData.id;
 
+	async function salvar()
+	{
+		const item = {
+			idItem: codigo
+		}
+	  alert("Item salvo");
+	}  
 
 	return (
 		<Layout>
@@ -78,31 +88,63 @@ const styles = StyleSheet.create({
 			>
 				<View style={styles.field}>
 					<Text>Código</Text>
-					<TextInput editable={false}>{codigo}</TextInput>
+					<TextInput editable={false} value={codigo}></TextInput>
 				</View>
 				<View style={styles.field}>
 					<Text>Marca</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<TextInput disabled value={marca} onChangeText={(val) => setMarca(val)}></TextInput>
 				</View>
 				<View style={styles.field}>
 					<Text>Nº De Serie</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<TextInput disabled value={numeroSerie} onChangeText={(val) => setNumeroSerie(val)}></TextInput>
 				</View>
 				<View style={styles.field}>
 					<Text>Data da compra</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<DatePicker
+							style={{width: 200}}
+							date={dtCompra}
+							mode="date"
+							placeholder="Selecione a data de compra"
+							format="DD-MM-YYYY"
+							minDate="2016-05-01"
+							maxDate="30-12-2999"
+							confirmBtnText="Confirmar"
+							cancelBtnText="Cancelar"
+							customStyles={{
+							dateIcon: {
+								position: 'absolute',
+								left: 0,
+								top: 4,
+								marginLeft: 0
+							},
+							dateInput: {
+								marginLeft: 36
+							}
+							}}
+							onDateChange={(date) => {setDtCompra(date)}}
+						/>
 				</View>
 				<View style={styles.field}>
-					<Text>Data final da garantia</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<Text>Data final da garantia</Text><DatePicker
+							date={dtGarantia}
+							mode="date"
+							placeholder="Selecione a data de compra"
+							format="DD-MM-YYYY"
+							minDate="2016-05-01"
+							maxDate="30-12-2999"
+							confirmBtnText="Confirmar"
+							cancelBtnText="Cancelar"
+							
+							onDateChange={(date) => {setDtGarantia(date)}}
+						/>
 				</View>
 				<View style={styles.field}>
 					<Text>Setor</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<TextInput disabled value={setor} onChangeText={(val) => setSetor(val)}></TextInput>
 				</View>
 				<View style={styles.field}>
 					<Text>Descrição</Text>
-					<TextInput disabled>{descricao}</TextInput>
+					<TextInput disabled value={descricao} onChangeText={(val) => setDescricao(val)}></TextInput>
 				</View>
 			</View>
 			
